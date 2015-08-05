e9c10cf3ca950f88d039744c7d8d810479e952d0
Daniel Griffin <dangriff50@gmail.com>
2015-08-05 16:50:54 -0400

    Step 6 adds filter and incomplete count

--- simple-todos.js
+++ simple-todos.js
@@ -1,14 +1,26 @@
-Tasks = new Mongo.Collection("tasks");
+Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
   Template.body.helpers({
     tasks: function(){
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      if (Session.get('hideCompleted')) {
+        // if hide completed is checked, filter out the tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // otherwise return all the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function() {
+      return Session.get('hideCompleted');
+    },
+    incompleteCount: function() {
+      return Tasks.find({'checked': {$ne: true}}).count();
     }
   });
 
   Template.body.events({
-    "submit .new-task": function(event){
+    'submit .new-task': function(event){
       // prevent the standard form submit event
       event.preventDefault();
 
@@ -22,18 +34,21 @@ if (Meteor.isClient) {
       });
 
       // clear form
-      event.target.text.value = "";
+      event.target.text.value = '';
+    },
+    'change .hide-completed input': function(event){
+      Session.set('hideCompleted', event.target.checked);
     }
   });
 
   Template.task.events({
-    "click .toggle-checked": function() {
+    'click .toggle-checked': function() {
       // set the checked property to the opposite of its current value
       Tasks.update(this._id, {
         $set: {checked: ! this.checked}
       });
     },
-    "click .delete": function(){
+    'click .delete': function(){
       Tasks.remove(this._id);
     }
   });
