8cb2432655bf08fb9e5ac357fec504d6cac2a2d3
unknown <dg39@VCL-NODE-18.win.duke.edu>
2015-08-05 13:00:10 -0400

    step 4 adds form to add tasks

--- simple-todos.js
+++ simple-todos.js
@@ -3,7 +3,27 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   Template.body.helpers({
     tasks: function(){
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
+
+  Template.body.events({
+    "submit .new-task": function(event){
+      // prevent the standard form submit event
+      event.preventDefault();
+
+      // get value from form element
+      var text = event.target.text.value;
+
+      // insert a task into the collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // clear form
+      event.target.text.value = "";
+    }
+  });
+
 }
