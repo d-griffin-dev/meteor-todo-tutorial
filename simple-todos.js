fe3f1b068c2ef30db3d187ffb7276cc95aea3c8c
Daniel Griffin <dangriff50@gmail.com>
2015-08-06 09:35:22 -0400

    Step 9 adds user accounts

--- simple-todos.js
+++ simple-todos.js
@@ -30,7 +30,9 @@ if (Meteor.isClient) {
       // insert a task into the collection
       Tasks.insert({
         text: text,
-        createdAt: new Date() // current time
+        createdAt: new Date(),            // current time
+        owner: Meteor.userId(),           // _id of logged in user
+        username: Meteor.user().username  // username of logged in user
       });
 
       // clear form
@@ -52,5 +54,9 @@ if (Meteor.isClient) {
       Tasks.remove(this._id);
     }
   });
+
+  Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_ONLY'
+  })
 
 }
